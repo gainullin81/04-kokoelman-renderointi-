d9a9919d3547fd8db02d45962f4a1f0ec3f0ebf5
alex Gainullin <againullin8@gmail.com>
2024-02-16 13:26:22 +0200

    Tapahtumankäsittelijän vieminen alikomponenttiin

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,30 @@
+
 import { useState } from 'react'
 
-const App = (props) => {
-  const [value, setValue] = useState(10)
+const Display = props => <div>{props.value}</div>
 
-  const setToValue = (newValue) => {
-    console.log('value now', newValue)
-    setValue(newValue)
-  }
-
+const Display = props => <div>{props.value}</div>
+  const Button = (props) => (
+    <button onClick={props.handleClick}>
+      {props.text}
+    </button>
+  )
+  const App = (props) => {
+    const [value, setValue] = useState(10)
+  
+    const setToValue = newValue => {
+      console.log('value now', newValue)
+      setValue(newValue)
+    }
+   
   return (
     <div>
       {value}
-      <button onClick={() => setToValue(1000)}>
-        thousand
-      </button>
-      <button onClick={() => setToValue(0)}>
-        reset
-      </button>
-      <button onClick={() => setToValue(value + 1)}>
-        increment
-      </button>
-      
+      <Button handleClick={() => setToValue(1000)} text="thousand" />
+      <Button handleClick={() => setToValue(0)} text="reset" />
+      <Button handleClick={() => setToValue(value + 1)} text="increment" />
     </div>
   )
 }
+
 export default App
